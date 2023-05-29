150657e09c37396a2a6153cfce1286d4debcb0b8
Salman544 <salmansz500@icloud.com>
2023-05-29 16:57:31 +0500

    Initial Commit

---
+++ PickUpApp/PickUpAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickUpAppApp.swift
+//  PickUpApp
+//
+//  Created by Muhammad Salman on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct PickUpAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
